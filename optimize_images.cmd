7c896cfccdc330b024001def967ae9f11ec5076a
Ivan Vityaev <godofluck89@gmail.com>
2024-11-02 20:18:49 +0300

    Update optimize_images.cmd

--- optimize_images.cmd
+++ optimize_images.cmd
@@ -7,21 +7,17 @@ set OUTPUT_DIR=images
 :: Set the cwebp command
 set CWEBP=.\bin\cwebp
 set QUALITY=85
+set TYPES=.png,.jpg
 
 :: Set rm command
 set RM=del
 
 :: Loop through all files in the input directory
-for %%f in (%INPUT_DIR%\*.png) do (
-  :: Convert PNG to WebP
-  %CWEBP% -q %QUALITY% -mt -o "%OUTPUT_DIR%\%%~nf.webp" "%%f"
-  %rm% "%%f"
-)
-
-for %%f in (%INPUT_DIR%\*.jpg) do (
-  :: Convert JPG to WebP
-  %CWEBP% -q %QUALITY% -mt -o "%OUTPUT_DIR%\%%~nf.webp" "%%f"
-  %rm% "%%f"
+for %%t in (%TYPES%) do (
+  for %%f in (%INPUT_DIR%\*%%t) do (
+    %CWEBP% -q %QUALITY% -mt -o "%OUTPUT_DIR%\%%~nf.webp" "%%f"
+    %rm% "%%f"
+  )
 )
 
 echo Conversion complete!
